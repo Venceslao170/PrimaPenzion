dc555d3d7f198dcc0d3e0aa78f9b3ee422e2d314
Václav <ceska.vasek@seznam.cz>
2024-04-11 14:31:21 +0200

    Update data.php

--- data.php
+++ data.php
@@ -34,12 +34,12 @@ class Stranka {
 			$prikaz = $GLOBALS["instancePDO"]->prepare("UPDATE stranka SET id=?, titulek=?, menu=?, obrazek=? WHERE id=?");
 		$prikaz -> execute(array($this->id,$this->titulek,$this->menu,$this->obrazek,$this->oldId));
 		}else{
-				//nejprve zjistit nejvysssi hodnotu poradi
+				
 			$prikaz = $GLOBALS["instancePDO"]->prepare("SELECT MAX(poradi) AS nejvyssi_poradi FROM stranka");
 			$prikaz->execute(array());
 			$vysledekDotazu = $prikaz->fetch();
 			$nejvyssiCislo = $vysledekDotazu["nejvyssi_poradi"];
-				//pote vlozit novou stranku do databaze
+				
 
 
 			$prikaz = $GLOBALS["instancePDO"]->prepare("INSERT INTO stranka SET id=?, titulek=?, menu=?, obrazek=?, poradi =?");
@@ -88,8 +88,7 @@ class Stranka {
 	}
 
 	function getObsah(){
-		//uvnitr classy neexistuje promenna $instancePDO
-		//musíme použít $GLOBALS["instancePDO] -- toto pole ma v sobe vsechny promenne nadefinovane mimo classu
+		
 		$prikaz = $GLOBALS["instancePDO"]->prepare("SELECT * FROM stranka WHERE id=?");
 		$prikaz -> execute(array($this->id));
 		$vysledekPrikazu = $prikaz -> fetch();
@@ -97,25 +96,19 @@ class Stranka {
 		if($vysledekPrikazu != NULL){
 			return $vysledekPrikazu ["obsah"];
 		}else {
-			//stranka nebyla v databazi nalezena
-			//vratime prazdny string
+			
 			return "";
-		}
-
-		//toto je stary zpusob, kdy jsme data meli v HTML
-		//return file_get_contents("./{$this->id}.html");
+		}		
 	}
 
 	function setObsah($argNovyObsah){
 		$prikaz = $GLOBALS["instancePDO"]->prepare("UPDATE stranka SET obsah=? WHERE id=?");
-		$prikaz -> execute(array($argNovyObsah,$this->id));		
-		
-		//return file_put_contents("./{$this->id}.html",$argNovyObsah);
+		$prikaz -> execute(array($argNovyObsah,$this->id));	
+			
 	}	
-
 }
 
-//predelat pole stranek, aby se data tahaly z databaze
+
 $poleStranek = array();
 $prikaz = $instancePDO -> prepare ("SELECT * FROM stranka ORDER BY poradi ASC");
 $prikaz -> execute(array());
@@ -126,41 +119,3 @@ foreach($poleVysledku AS $vysledek){
 }
 
 
-/* -- data ze souborů
-$poleStranek = array(
-	"domu" => new Stranka ("domu", "PrimaPenzion", "Domů", "primapenzion-main.jpg"),
-	"galerie" => new Stranka ("galerie","Fotogalerie","Fotky","primapenzion-pool-min.jpg"),
-	"rezervace" => new Stranka ("rezervace","Rezervace","Chci pokoj","primapenzion-room.jpg"),
-	"kontakt" => new Stranka ("kontakt","Kontakt","Napište nám","primapenzion-room2.jpg"),
-)
-*/
-
-/*
-$poleStranek = array(
-        "domu" => [
-            "id" => "domu",
-            "titulek" => "PrimaPenzion",
-            "menu" => "Domů",
-            "obrazek" => "primapenzion-main.jpg"
-        ],
-        "galerie" => [
-            "id" => "galerie",
-            "titulek" => "Fotogalerie",
-            "menu" => "Fotky",
-            "obrazek" => "primapenzion-pool-min.jpg"
-        ],
-        "rezervace" => [
-            "id" => "rezervace",
-            "titulek" => "Rezervace",
-            "menu" => "Chci pokoj",
-            "obrazek" => "primapenzion-room.jpg"
-        ],
-        "kontakt" => [
-            "id" => "kontakt",
-            "titulek" => "Kontakt",
-            "menu" => "Napište nám",
-            "obrazek" => "primapenzion-room2.jpg"
-        ],
-    );
-
-*/
